b99302c7aa4e1f9f521ce990c358d5949097be17
Naoki Sugimoto <assoonasp_t@yahoo.co.jp>
2016-10-04 02:13:22 +0900

    initial commit

---
+++ RandomWiki.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'nokogiri'
+require 'openssl'
+
+def wiki(num)
+  result = []
+  num.times do |i|
+    charset = nil
+    url = 'https://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA'
+    html = open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE) do |f|
+      charset = f.charset
+      f.read
+    end
+    print "*"
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    doc.title =~ /\w*(?= - Wikipedia)/
+    result << $`
+  end
+  puts ""
+  puts result
+end
+
+wiki 5
